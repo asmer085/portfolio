703f03c7f9d1bfabf71c2fe35a90f4e7bad273d2
asmer085 <129118617+asmer085@users.noreply.github.com>
2023-07-05 19:54:42 +0200

    reset

--- src/data/headerData.js
+++
@@ -1,7 +0,0 @@
-
-export const headerData = {
-    name: 'Asmer Karabeg',
-    title: "Student",
-    desciption:"The only true wisdom is in knowing you know nothing. The measure of intelligence is the ability to change. The function of education is to teach one to think intensively and to think critically. Intelligence plus character - that is the goal of true education.",
-    image: 'https://i.imgur.com/64vhUTt.jpeg',
-}
--- src/data/socialsData.js
+++
@@ -1,4 +0,0 @@
-export const socialsData = {
-    github: 'https://github.com/asmer085',
-    linkedIn: 'https://www.linkedin.com/in/asmer-karabeg-6ba619269/',
-}
